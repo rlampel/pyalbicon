116286cf4495d5fb8c87988e7151b8dab2f47f9d
Robert Lampel <robert.lampel@ovgu.de>
2025-08-27 16:10:36 +0200

    added random high-dimensional benchmark

---
+++ ODELifting/apps/bvp_42.py
@@ -0,0 +1,53 @@
+import casadi as cs
+import numpy as np
+from . import base_class
+
+
+class Problem(base_class.BVP):
+    lamb = 0.05
+    x_dim = 30
+    seed = 42
+
+    def __init__(self):
+        base_class.BVP()
+
+    def get_ode(self):
+        # Declare model variables
+        x = cs.MX.sym('x', self.x_dim)
+        t = cs.MX.sym('t', 1)
+
+        # Model equations
+        xdot = cs.DM([])
+        np.random.seed(self.seed)
+        for i in range(self.x_dim):
+            b = cs.DM(np.random.rand(self.x_dim))
+            b = cs.DM([1.] * self.x_dim) - 2 * b
+            c = 1 - 2 * float(np.random.rand(1))
+            xdot_i = b.T @ x + c
+            xdot = cs.vertcat(xdot, xdot_i)
+
+        # Objective term
+        ode = {'x': x, 'ode': xdot, 't': t}
+        return ode
+
+    def get_boundary_fct(self):
+        x_start = cs.MX.sym('xs', self.x_dim)
+        x_end = cs.MX.sym('xe', self.x_dim)
+
+        # declare boundary function
+        bvs = x_end - cs.DM([1] * self.x_dim)
+        R = cs.Function('R', [x_start, x_end], [bvs], ['xs', 'xe'], ['bvp'])
+        return R
+
+    def get_init(self):
+        init = {}
+        init["s_start"] = cs.DM([0.] * self.x_dim)
+        init["s_dim"] = self.x_dim
+        return init
+
+    def get_grid_details(self):
+        min_t = 0
+        max_t = 5
+        num_lifts = 20
+        return min_t, max_t, num_lifts
+
